3e89ca169415bdeb5d16742e0f42214d3a95a816
unalsrmr <unalsariemir@hmail.com>
2025-03-16 18:17:25 +0300

    select group by filter

--- 4.hafta/my_script.py
+++ 4.hafta/my_script.py
@@ -60,10 +60,12 @@ import numpy as np
 
 # endregion
 
-df = pd.read_csv(
-    filepath_or_buffer="data/imdb.csv",
-    encoding="utf-8"
-)
+
+# region Select-Filter
+# df = pd.read_csv(
+#     filepath_or_buffer="data/imdb.csv",
+#     encoding="utf-8"
+# )
 # print(df.shape)
 # print(df.head(2))
 # print(df.describe())
@@ -76,7 +78,97 @@ df = pd.read_csv(
 # # Path 3
 # print(df.loc[0:20, ["Title","Year"]])
 
+
+#rating 8.0 dan büyük olan filmlerin
+#select-> Title Rating Year
+#20-50 arasıdnaki filmler gelsin
+#sort edelim
+# print(
+#     df[df["Rating"]>8.0]
+#     [["Title","Rating","Year"]][20:50].sort_values(by="Rating",ascending=True)
+#     )
+
+# #Year 2014- 2019 arasında olan
+# #Title,Rating,Year
+
+# print(
+#     df[(df["Year"]>=2014)&(df["Year"]<=2019)][["Title","Year"]]
+# )
+
+# # metascore 80 den büyük olanlar ve ratingi 8.0 dan büyük olanlar
+# # Title, Rating ,Year,
+
+# print(
+#     df[(df["Metascore"]>=80)&(df["Rating"]>=8.0)][["Title","Rating","Metascore"]].sort_values(by="Metascore",ascending=True)
+# ) 
+
+# endregion
+
+#region NBA Group-by Select Filter
+# df = pd.read_csv(filepath_or_buffer="data/nba.csv",encoding="utf-8")
+
+# print(
+#     df[(df["Age"]>=25)&(df["Salary"]>=10000000)][["Name","Age"]].sort_values(by="Age",ascending=True)
+
+# )
+
+# #en yüksek maaşlı oyuncu
+# print(
+#     df[(df["Salary"])==(df["Salary"].max())][["Name","Age"]]
+# )
+
+# #group by
+# print(
+#     df.groupby(by="Position")["Salary"].sum()
+# )
+# print(
+#     df.groupby(by="Team")["Salary"].sum().sort_values(ascending=True)
+# )
+
+# #kaç tane takım varsa onun sayısını döner
+# print(
+#     df["Team"].nunique()
+# )
+# print(
+#     df.groupby(by="Team")["Team"].count()
+# )
+# result = df.groupby(by="Team")
+# print(result.value_counts().to_string)# benzersiz satırların kaç kez tekrarlandığını ekrana yazar
+
+# #yaşları 25-35 arasında olan adı takımı bilgilerini listele
+# print(
+#     df[(df["Age"]<=35)&(df["Age"]>=25)][["Name","Team"]]
+# )
+# print(
+#     df[(df["Age"].between(25,35))][["Name","Team","Age"]].sort_values(by="Age" ,ascending=True)
+# )
+# result = df[(df["Age"].between(25,35))][["Name","Team","Age"]].sort_values(by="Age" ,ascending=True)
+# print(result.groupby(by="Team").size())
+# print(result.groupby(by="Team")["Age"].mean()) #yaş ortalaması hesaplar
+
+# #custom funtion
+# def str_find(name):
+#     if isinstance(name, str):  # Eğer name bir string ise
+#         return "and" in name
+#     return False  # Eğer name string değilse False döner
+
+# print(df["Name"].apply(str_find))
+# print(df["Name"].apply(lambda x: str_find(x)))
+# endregion
+
+
+# region Youtube
+df = pd.read_csv(filepath_or_buffer="data/youtube.csv",encoding="utf-8")
+
+# print(df.shape)
+# print(df.columns)
+
+
+#Bu kod, bir Pandas DataFrame'deki verileri "Grade" sütununa göre gruplar, 
+#her grubun "Views" sütunundaki toplamını hesaplar, bu toplamları artan sırada sıralar
+#ve en düşük 10 sonucu ekrana yazdırır.
 print(
-    df[df["Rating"]>8.0]
-    [["Title","Rating","Year"]][20:].sort_values(by="Rating",ascending=True)
+    df.groupby(by="Grade")[["Views"]].sum().sort_values(by="Views",ascending=True).head(10)
     )
+# endregion
+
